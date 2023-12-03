85c2caf47466ae8f5895fd15e7bc792245fcabac
Davi <dumwebtecnologia@gmail.com>
2023-12-03 16:50:17 -0300

    commit Edson

--- Conversor.py
+++ Conversor.py
@@ -1,1 +1,9 @@
 # Funcão de Conversor de Temperatura.
+def cel_fah(cel):
+    return 9 * cel / 5 + 32
+
+
+# Teste
+# print(f'{0}ºC = {cel_fah(0)}ºF')
+# print(f'{-40}ºC = {cel_fah(-40)}ºF')
+# print(f'{38}ºC = {cel_fah(38)}ºF')
